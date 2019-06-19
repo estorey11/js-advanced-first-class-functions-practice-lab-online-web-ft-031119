1c072b026f5b4132bf54e98f90dd09d7fe1e8563
Eric Storey <estorey@gmail.com>
2019-06-19 16:38:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ const driversByName = function (drivers) {
   });
 };
 
+const reduceRevenue = function (agg, driver, i, arr) {
+  console.log('The aggregate up to this point is:', agg);
+  console.log("The current element's name is:", el.name);
+  console.log('The index of the current element is:', i);
+  console.log('----------------');
+ 
+  return [...agg, el.name];
+};
+
 const totalRevenue = function (drivers) {
   return drivers.slice().sort(function (driver1, driver2) {
     return driver1.revenue-driver2.revenue
