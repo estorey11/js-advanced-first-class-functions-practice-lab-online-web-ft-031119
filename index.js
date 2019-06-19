055254acf0297a03f62b673aab580a2c2574b5c2
Eric Storey <estorey@gmail.com>
2019-06-19 16:35:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ const driversByRevenue = function (drivers) {
   });
 };
 
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driver1, driver2) {
+    return driver1.name.localeCompare(driver2.name)
+  });
+};
+
 
 
 
